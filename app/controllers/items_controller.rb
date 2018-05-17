bdecc55be04e4bc5f04a1d46378970368e84cb97
Ryan Leonel <ryan.leonel@mokapos.com>
2018-05-17 19:14:15 +0700

    [MOKA-001] Finish the implementation of Create/Update/Delete for Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 
+  # GET /items
   def index
     # List all of the Items that are owned by the logged in User's Business
     if current_business.present?
@@ -10,15 +11,50 @@ class ItemsController < ApplicationController
     end
   end
 
+  # GET /items/new
+  def new
+    @item = Item.new
+  end
+
+  # GET /items/:id/edit
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  # POST /items
   def create
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item = Item.new(item_params)
+    @item.business = current_business
+    if @item.save
+      redirect_to items_path, notice: "Item #{@item.name} has been successfully created"
+    else
+      render 'new'
+    end
   end
 
-  def Update
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+  # PUT /items/:id
+  def update
+    @item = Item.find(params[:id])
+    if @item.update_attributes(item_params)
+      redirect_to items_path, notice: "Item #{@item.name} has been successfully updated"
+    else
+      render 'edit'
+    end
   end
 
+  # DELETE /items/:id
   def destroy
-    # TODO: [MOKA-001] Finish the implementation of Create/Update/Delete for Items
+    @item = Item.find_by(id: params[:id])
+    if @item.try(:destroy)
+      redirect_to items_path, notice: "Item #{@item.name} has been successfully deleted"
+    else
+      redirect_to items_path, alert: "Failed to delete item #{@item.name}"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
